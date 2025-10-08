0f528718b930462dd512f30ee8d3324dd15cd686
misana9 <once.jd2@gmail.com>
2025-10-08 11:02:00 +0800

    functionality done,layout done, design in progress

--- src/index.js
+++ src/index.js
@@ -1,33 +1,45 @@
+import './styles.css'
+
 const searchBox = document.getElementById("searchCity")
 const submit = document.getElementById("searchBtn")
 const locationTag = document.getElementById("location")
+const locationCondition = document.getElementById("locationCondition")
 const locationTemp = document.getElementById("locationTemp")
-let locations = "";
+const locationHumidity = document.getElementById("locationHumidity")
+const loading = document.getElementById("loading")
+const forecastBox = document.getElementById("forecastBox")
 
 const forecasted = async function getForecast(locations){
     const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locations}/?key=LC9P9NEQJ2XQBBM4HXGWW3PQN`
     const response = await fetch(url)
     if(!response.ok) throw new Error("failed to fetch weather data");
     const data = await response.json();
-    return (data.currentConditions.temp)
+    return (data)
 }
 
-function renderLocation(locations,temp){
+function renderLocation(locations,temp,condition,humidity){
+    forecastBox.style.display = "flex";
     locationTag.textContent = `Location: ${locations}`;
-    locationTemp.textContent = "Temp:" + temp;
+    locationCondition.textContent = "Condition: " + condition
+    locationTemp.textContent = "Temp: " + temp;
+    locationHumidity.textContent = "Humidity: " + humidity
 }
 
 submit.addEventListener("click", async (e) => {
   e.preventDefault();
   const locations = searchBox.value.trim();
-  if(!location) return alert("Please enter a location")
+  if(!locations) return alert("Please enter a location")
 
     try{
-        const temp = await forecasted(locations);
-        renderLocation(locations, temp);
+        loading.style.display = "block";
+        const forecast = await forecasted(locations);
+        console.log(forecast)
+        renderLocation(forecast.resolvedAddress,forecast.currentConditions.temp,forecast.currentConditions.conditions,forecast.currentConditions.humidity);
+        
     }catch(err){
-        locationTag.textContent = "Error fetching weather data.";
-        locationTemp.textContent = "";
+        renderLocation("Error","Error","Error","Error")
+    }finally{
+        loading.style.display = "none";
     }
 });
 
